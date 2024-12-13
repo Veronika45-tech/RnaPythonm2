59b556730d590077dd4f96bb91296e758ea80b59
Veronika45-tech <veronikaandreushkova@icloud.com>
2024-12-13 23:11:44 +0300

    Update task.py

--- Лабораторная 1/task.py
+++ Лабораторная 1/task.py
@@ -1,27 +1,32 @@
-class Phone:
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
+
+
+class SmartPhone:
     """
-    Класс, представляющий телефон
+    Класс, представляющий смартфон
     Атрибуты:
-    - model (str): Модель телефона
-    - manufacturer (str): Производитель телефона
-    - year_of_manufacture (int): Год выпуска телефона
+    - model (str): Модель смартфон
+    - manufacturer (str): Производитель смартфона
+    - year_of_manufacture (int): Год выпуска смартфона
     """
 
     def __init__(self, model: str, manufacturer: str, year_of_manufacture: int):
         """
-        Конструктор класса Phone
+        Конструктор класса SmartPhone
         Параметры:
-        - model: Модель телефона
-        - manufacturer: Производитель телефона
-        - year_of_manufacture: Год выпуска телефона
-        >>> phone = Phone("iPhone X", "Apple Inc.", 2023)
+        - model: Модель смартфон
+        - manufacturer: Производитель смартфона
+        - year_of_manufacture: Год выпуска смартфона
+        >>> smartphone = SmartPhone("iPhone X", "Apple Inc.", 2023)
         """
         if not isinstance(model, str) or len(model.strip()) == 0:
-            raise ValueError("Модель телефона должен быть непустой строкой.")
+            raise TypeError("Модель смартфона должен быть непустой строкой.")
         if not isinstance(manufacturer, str) or len(manufacturer.strip()) == 0:
-            raise ValueError("Производитель телефона должен быть непустой строкой.")
+            raise TypeError("Производитель смартфона должен быть непустой строкой.")
         if not isinstance(year_of_manufacture, int) or year_of_manufacture < 1900:
-            raise ValueError("Год выпуска телефона должен быть больше или равен 1900.")
+            raise TypeError("Год выпуска смартфона должен быть больше или равен 1900.")
 
         self.model = model
         self.manufacturer = manufacturer
@@ -29,32 +34,27 @@ class Phone:
 
     def get_model(self) -> str:
         """
-        Метод для получения модели телефона
-        :return: Модель телефона
+        Метод для получения модели смартфона
+        :return: Модель смартфона
         """
         return self.model
 
     def get_manufacturer(self) -> str:
         """
-        Метод для получения производителя телефона
-        :return: Производитель телефона
+        Метод для получения производителя смартфона
+        :return: Производитель смартфона
         """
         return self.manufacturer
 
     def get_year_of_manufacture(self) -> int:
         """
-        Метод для получения года выпуска телефона
-        :return: Год выпуска телефона.
-        >>> phone = Phone("iPhone X", "Apple Inc.", 2023)
-        >>> phone.get_year_of_manufacture()
+        Метод для получения года выпуска смартфона
+        :return: Год выпуска смартфона
+        >>> smartphone = SmartPhone("iPhone X", "Apple Inc.", 2023)
+        >>> smartphone.get_year_of_manufacture()
         2023
         """
         return self.year_of_manufacture
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod(verbose=True)
 
 
 class Tree:
@@ -125,20 +125,21 @@ class Car:
     Представляет автомобиль с маркой, моделью, пробегом и уровнем топлива
 
     Атрибуты:
-        make (str): Марка автомобиля
+        mark (str): Марка автомобиля
         model (str): Модель автомобиля
         mileage (int): Общий пройденный путь автомобилем в километрах. Должен быть неотрицательным
         fuel_level (float): Текущий уровень топлива в литрах. Должен находиться между 0 и емкостью бензобака
     """
 
     TANK_CAPACITY = 60  # литры
+    KM_PER_LITER = 10  # Количество километров, которое автомобиль может проехать на 1 литр топлива
 
-    def __init__(self, make: str, model: str, mileage: int = 0, fuel_level: float = 0.0):
+    def __init__(self, mark: str, model: str, mileage: int = 0, fuel_level: float = 0.0):
         """
         Инициализирует объект Car
 
         Аргументы:
-            make (str): Марка автомобиля
+            mark (str): Марка автомобиля
             model (str): Модель автомобиля
             mileage (int, optional): Начальный пробег. По умолчанию равен 0
             fuel_level (float, optional): Начальный уровень топлива. По умолчанию равен 0.0.
@@ -146,33 +147,33 @@ class Car:
         Исключения:
             ValueError: Если пробег отрицательный или уровень топлива выходит за пределы допустимого диапазона.
         """
-        self.make = make
+        self.mark = mark
         self.model = model
         if mileage < 0:
             raise ValueError("Пробег не может быть отрицательным")
         self.mileage = mileage
         if fuel_level < 0 or fuel_level > self.TANK_CAPACITY:
-            raise ValueError(f"Уровень топлива должен быть между 0 и {self.TANK_CAPACITY}.")
+            raise TypeError(f"Уровень топлива должен быть между 0 и {self.TANK_CAPACITY}.")
         self.fuel_level = fuel_level
 
     def drive(self, distance: int) -> None:
         """
-        Ведет автомобиль на указанное расстояние, обновляя пробег и уровень топлива
+        Ведет автомобиль на указанное расстояние, обновляя пробег и уровень топлива.
 
         Аргументы:
-            distance (int): Расстояние в километрах, которое необходимо проехать. Должно быть положительным
+            distance (int): Расстояние в километрах, которое необходимо проехать. Должно быть положительным.
 
         Исключения:
-            ValueError: Если расстояние не положительное
-
+            ValueError: Если расстояние не положительное.
         """
         if distance <= 0:
             raise ValueError("Расстояние должно быть положительным")
+
         self.mileage += distance
-        fuel_consumption = distance / 10
+        fuel_consumption = distance / self.KM_PER_LITER
+
         if fuel_consumption > self.fuel_level:
-            raise ValueError("Недостаточно топлива для преодоления данного расстояния")
-        self.fuel_level -= fuel_consumption
+            raise TypeError("Недостаточно топлива для преодоления данного расстояния")
 
     def refill_tank(self) -> float:
         """
@@ -192,7 +193,7 @@ class Car:
             dict: Словарь, содержащий марку, модель, пробег и уровень топлива автомобиля
         """
         return {
-            "make": self.make,
+            "mark": self.mark,
             "model": self.model,
             "mileage": self.mileage,
             "fuel_level": self.fuel_level
